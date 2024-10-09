b3765e6abb779859b1434229c72815f65907e7f3
DarkPh0eNixKrg <fatrus.s0dtl@gmail.com>
2024-10-09 17:05:27 +0500

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\GetHostIp;
+
+use APP;
+use CController as CAction;
+
+class Module extends \Core\CModule {
+
+    public function init(): void {
+    }
+
+    public function onBeforeAction(CAction $action): void {
+    }
+
+    public function onTerminate(CAction $action): void {
+    }
+}
